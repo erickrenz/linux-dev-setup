c8340660c55beab8a1e95edb2c30348c0776a604
erickrenz <krenz.eric.krenz@gmail.com>
2023-03-11 13:30:42 +0100

    update installation script

--- README.md
+++ README.md
@@ -1,10 +1,33 @@
 # linux-dev-setup
 
-A simple script to install developer tools for Fedora/CentOS/RHEL based Linux distros using the `dnf` package manager.
+A simple script to install developer tools for Fedora/CentOS/RHEL based Linux distros using the `dnf` package manager. 
+
+This goal of this script is to quickly get a development system up and running by installing all of the required packages needed for embedded or linux kernel development. This script can also be applied to any low level development projects in general. No prerequisites are required other than running a RHEL (Red Hat Enterprise Linux) based Linux distro. 
 
 ## Getting Started
 
 ```bash
-wget https://raw.githubusercontent.com/erickrenz/linux-dev-setup/main/setup.sh \
--O setup.sh && sudo bash setup.sh
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/erickrenz/linux-dev-setup/main/setup.sh)"
 ```
+
+## Installed Tools
+
+`Git` &nbsp;
+`Zsh` &nbsp;
+`ohmyzsh` &nbsp;
+`GNU C` &nbsp;
+`GNU Make` &nbsp;
+`bash` &nbsp;
+`flex` &nbsp;
+`bison` &nbsp;
+`openssl` &nbsp;
+`bindgen` &nbsp;
+`binutils` &nbsp;
+`Kernel-devel` &nbsp;
+`Kernel-headers` &nbsp;
+`LLVM` &nbsp;
+`Clang` &nbsp;
+`Rust` &nbsp;
+`Qemu` &nbsp;
+`VSCode` &nbsp;
+`Neovim` &nbsp;
--- setup.sh
+++ setup.sh
@@ -1,3 +1,60 @@
 #!/bin/bash
 
-dnf update -y
+# Detect package manager
+if [[ -e /bin/dnf || -e /usr/bin/dnf ]]; then
+    dnf check-update -y
+    dnf upgrade -y
+    dnf update -y
+else
+    echo "The dnf package manager is required for this script."
+    exit
+fi
+
+### git ###
+dnf install -y git
+
+### zsh ###
+# Sets zsh as default shell and configures oh-my-zsh
+# https://ohmyz.sh#install
+dnf install -y zsh chsh
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+### Kernel Tools ###
+# https://www.kernel.org/doc/html/latest/process/changes.html
+dnf install -y dwarves bindgen python3-sphinx openssl pcmciautils grub-customizer
+# https://packages.fedoraproject.org/pkgs/kernel/kernel/
+dnf install -y kernel kernel-devel kernel-headers kernel-cross-headers
+
+### LLVM / Clang ###
+# https://packages.fedoraproject.org/pkgs/llvm/llvm/
+dnf install -y llvm clang cmake lld lldb clang-tools-extra
+
+### Rust ###
+# https://www.rust-lang.org/tools/install
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+### Qemu ###
+# https://www.qemu.org/download/#linux
+dnf install -y qemu qemu-kvm @virtualization
+
+### VSCode ###
+# https://code.visualstudio.com/docs/setup/linux
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update -y
+dnf install -y code
+
+### Neovim ###
+# https://github.com/neovim/neovim/wiki/Installing-Neovim
+dnf copr enable -y agriffis/neovim-nightly
+dnf install -y neovim python3-neovim
+
+### Miscelaneous ###
+# Install groups are the "build-essential" equivalent for Fedora
+dnf group install -y "C Development Tools and Libraries" "Development Tools"
+# Install Podman (docker equivalent)
+dnf install -y podman
+
+### Update installed tools ###
+dnf update -y 
