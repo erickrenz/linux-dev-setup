2dbbc42c4a20473fa5205e0e41565fef0b7f4b57
Eric Krenz <krenz.eric.krenz@gmail.com>
2023-04-17 19:26:28 +0200

    update dependencies script

--- ansible-setup.sh
+++ ansible-setup.sh
@@ -1,2 +1,12 @@
-#!/bin/sh
-sudo dnf install -y python ansible
+#!/bin/bash
+
+# Detect package manager == dnf
+if [[ -e /bin/dnf || -e /usr/bin/dnf ]]; then
+    dnf upgrade -yq
+else
+    echo "The dnf package manager is required for this script."
+    exit
+fi
+
+# Install andible dependencies
+dnf install -yq python ansible
