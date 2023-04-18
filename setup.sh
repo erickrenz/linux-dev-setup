2ccecf2c9408ef76c71cf54dbac911106d202fe4
Eric Krenz <krenz.eric.krenz@gmail.com>
2023-04-18 07:56:19 +0200

    call run playbook from install script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Verify package manager
 if [[ -e /bin/dnf || -e /usr/bin/dnf ]]; then
-    echo "[1/3] Updating packages..."
+    echo "[1/4] Updating packages..."
     dnf upgrade -yq
 else
     echo "The dnf package manager is required for this script."
@@ -10,9 +10,13 @@ else
 fi
 
 # Install andible dependencies
-echo "[2/3] Installing dependencies..."
+echo "[2/4] Installing dependencies..."
 dnf install -yq git ansible
 
 # Clone this repository
-echo "[3/3] Cloning repository..."
+echo "[3/4] Cloning repository..."
 git clone -q https://github.com/erickrenz/linux-dev-setup.git
+
+# Run ansible playbook
+echo "[4/4] Running playbook..."
+ansible-playbook linux-dev-setup/playbook.yaml
